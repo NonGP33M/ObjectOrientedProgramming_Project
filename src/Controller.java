a6c34cc66fc0f6c3e947ec0521f234da2c20cab3
Rattanaporn <88606135+RSMMIX@users.noreply.github.com>
2022-04-20 22:37:14 +0700

    update

---
+++ src/Controller.java
@@ -0,0 +1,59 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.web.WebView;
+
+public class Controller {
+
+    @FXML
+    private Button btBack;
+
+    @FXML
+    private Button btForward;
+
+    @FXML
+    private Button btOption;
+
+    @FXML
+    private Button btRefresh;
+
+    @FXML
+    private Button btZoomIn;
+
+    @FXML
+    private Button btZoomOut;
+
+    @FXML
+    private WebView webView;
+
+    @FXML
+    void Option(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goBack(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goForward(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goRefresh(ActionEvent event) {
+
+    }
+
+    @FXML
+    void zoomIn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void zoomOut(ActionEvent event) {
+
+    }
+
+}
---
+++ src/TabContro.java
@@ -0,0 +1,9 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.TabPane;
+
+public class TabContro {
+
+    @FXML
+    private TabPane add;
+
+}
