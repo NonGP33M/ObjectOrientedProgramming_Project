8a804b1486fbb36e9db97c56e13310e372c4d20e
Rattanaporn <88606135+RSMMIX@users.noreply.github.com>
2022-04-20 23:40:46 +0700

    update

--- src/App.java
+++ src/App.java
@@ -1,26 +1,22 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 
 public class App extends Application{
     public static void main(String[] args) throws Exception {
         launch(args);
+        
     }
 
     @Override
-    public void start(Stage arg0) throws Exception {
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("LoanGUI.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Program");
+        primaryStage.setScene(scene);
+        primaryStage.show();
         
-        
-    }
-
-    public void showText(String title,Stage window,String text){
-        TextArea root = new TextArea(text);
-        Scene secondScene = new Scene(root,600,400);
-        Stage secondWindow = new Stage();
-        secondWindow.setTitle(title);
-        secondWindow.setScene(secondScene);
-        secondWindow.initOwner(window);
-        secondWindow.show();
     }
 }
